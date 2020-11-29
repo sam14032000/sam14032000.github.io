b06f5cd0bcd42053ecae406e0cfb80dfcb402aa8
Saksham Agrawal <31131966+sam14032000@users.noreply.github.com>
2020-11-29 23:59:26 +0530

    Update and rename custom.html to gtag.js

---
+++ _includes/analytics-providers/gtag.js
@@ -0,0 +1,11 @@
+<!-- start custom analytics snippet -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VDKPV1LBTZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VDKPV1LBTZ');
+</script>
+<!-- end custom analytics snippet -->
